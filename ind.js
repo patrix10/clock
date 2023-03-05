e58d47ae050b796de8e39c11964a6386b5b436e7
patrix10 <125147974+patrix10@users.noreply.github.com>
2023-03-05 12:36:31 +0530

    Add files via upload

---
+++ ind.js
@@ -0,0 +1,22 @@
+function displaytime(){
+    time=new Date();
+    console.log(time);
+    document.getElementById('time').innerHTML=time;
+}
+setInterval(displaytime,);
+
+const degree = 6;
+const hr = document.querySelector('#hr');
+const min = document.querySelector('#min');
+const sec = document.querySelector('#sec');
+
+setInterval(()=>{
+    const date = new Date();
+    const hh = date.getHours() * 30;
+    const mm = date.getMinutes() * degree;
+    const ss = date.getSeconds() * degree;
+
+    hr.style.transform = `rotateZ(${hh + (mm/12)}deg)`;
+    min.style.transform = `rotateZ(${mm}deg)`;
+    sec.style.transform = `rotateZ(${ss}deg)`;
+});
